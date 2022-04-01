3e643b957bcabf7b7b5bc602372da49130777897
abrormirzakarimov <102176504+abrormirzakarimov@users.noreply.github.com>
2022-04-01 18:27:16 +0500

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,7 +4,5 @@ from loader import dp
 from keyboards.default import menu_keybord
 @dp.message_handler(CommandStart())
 async def bot_start(message: Message):
-   info = f"Assalomu aleykum, {message.from_user.full_name}\n"
-   info+= f"AL-QURAN  botiga xush kelibsiz!"
-   await message.answer(info,reply_markup=menu_keybord)
+   await message.answer(f"Salom ",reply_markup=menu_keybord)
 
