cba89623471f6977d4543e5ce3514d9454b23ca8
abrormirzakarimov <102176504+abrormirzakarimov@users.noreply.github.com>
2022-04-01 18:09:15 +0500

    Add files via upload

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,10 @@
-from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
-
+from aiogram.types  import Message
 from loader import dp
-
-
+from keyboards.default import menu_keybord
 @dp.message_handler(CommandStart())
-async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+async def bot_start(message: Message):
+   info = f"<b>Assalomu aleykum, {message.from_user.full_name}\n</b>"
+   info+= f"<b>AL-QURAN  botiga xush kelibsiz! </b>"
+   await message.answer(info,reply_markup=menu_keybord)
+
